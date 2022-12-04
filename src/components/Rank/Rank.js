be9ffc30bc954e30e293bdf7b2172ed65ebb37c6
HeriYantodotDev <heri@Tukang_Logika>
2022-12-04 21:31:58 +0700

    adding components + edit manifest.json()

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="text-light">
+                {`Heri, your current rank is ...`}
+            </div>
+            <div className="text-light fs-1">
+                {`#5`}
+            </div>
+        </div>
+    )
+}
+
+export default Rank
