b949698d072847eb3db4dc80422ffecbe7b5d2de
HeriYantodotDev <heri@Tukang_Logika>
2022-12-16 10:54:59 +0700

    code review: frontend

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,8 +7,8 @@ const Nav = ({onChangeRoute, resetImageUrl, isSignIn}) => {
             <ul className="nav justify-content-end">
                 <li className="nav-item">
                     <p onClick={()=> {
-                        onChangeRoute('signin');
                         resetImageUrl();
+                        onChangeRoute('signin');
                     }} className="pointer nav-link active link-warning fs-2" aria-current="page" >Sign Out</p>
                 </li>
     
